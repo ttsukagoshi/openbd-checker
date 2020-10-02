cf1b36624f85d9a02034817c0b742eb04ae49401
ttsukagoshi <tsukag26@gmail.com>
2020-10-02 17:51:49 +0900

    openBDを別タブで参照する原型

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# chrome-ext_openBD-checker
+# openBD情報チェッカー（Google Chrome拡張機能）
+ウェブブラウザ（Chrome）内で選択したISBNコードに基づいて、コンテクストメニュー（右クリックメニュー）から[openBD](https://openbd.jp/)に登録されている情報を参照できるようにする拡張機能。
+
+## インストール
+hogehoge
+
+## 使用
+fugafuga
+- ハイフン（-）が入ったISBN文字列でも検索可能。
+
+## 参考
+- [図書館員のための個人プロジェクトによる学術情報システムスキルアップ](https://www.jstage.jst.go.jp/article/jkg/69/4/69_150/_pdf/-char/ja)
--- src/background.js
+++ src/background.js
@@ -1,14 +1,33 @@
+// Global variable(s)
+const OPENBD_API_VERSION = 'v1'; // openBDのAPIバージョン。 https://openbd.jp/
+
 chrome.runtime.onInstalled.addListener(function () {
-    chrome.storage.sync.set({ color: '#3aa757' }, function () {
-        console.log("The color is green.");
-    });
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostEquals: 'developer.chrome.com' },
-            })
-            ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
+    chrome.contextMenus.create({
+        "id": "openBDChecker",
+        "title": "openBDを検索する（ISBN）",
+        "type": "normal",
+        "contexts": [
+            "selection",
+            "editable"
+        ]
     });
 });
+
+chrome.contextMenus.onClicked.addListener(function (info) {
+    if (info.menuItemId == "openBDChecker") {
+        openBDCheckerAlert(info);
+    }
+});
+
+function openBDCheckerAlert(info) {
+    if (!info.selectionText) {
+        alert('ISBNコードが選択されていません。');
+    }
+    var isbnText = info.selectionText.replace(/\-/g, '');
+    var baseUrl = `https://api.openbd.jp/${OPENBD_API_VERSION}/get`;
+    var url = baseUrl + `?isbn=${encodeURIComponent(isbnText)}`;
+    chrome.tabs.create({
+        'url': url,
+        'active': true
+    });
+}
