3a3788a7f2c5b67362b27eb0176f159c002a3255
t tsukagoshi <tsukag26@gmail.com>
2020-10-06 02:56:45 +0900

    v1 complete for local development

--- src/background.js
+++ src/background.js
@@ -14,24 +14,40 @@ chrome.runtime.onInstalled.addListener(function () {
 
 chrome.contextMenus.onClicked.addListener(function (info) {
     if (info.menuItemId == "openBDChecker") {
-        openBDCheckerAlert(info);
+        openBDChecker(info);
     }
 });
 
-function openBDCheckerAlert(info) {
-    console.log(info);
+function openBDChecker(info) {
     try {
         if (!info.selectionText) {
             throw new Error('ISBNが選択されていません。');
         }
-        let isbnText = info.selectionText.replace(/\-/g, '');
+        let isbnText = info.selectionText.replace(/\-|\,/g, '');
         /*
         verify isbnText
         */
         let url = `https://api.openbd.jp/${OPENBD_API_VERSION}/get?isbn=${encodeURIComponent(isbnText)}`;
-        chrome.storage.local.set({'openBdUrl': url}, function() {
-            console.log(`openBD URL set to ${url}`);
+        chrome.storage.local.set({ 'openBdUrl': url }, function () {
+            console.log(`openBdUrl set to ${url}`);
         });
+        // openBD APIからGETする。
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url)
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                let status = xhr.status;
+                if (status >= 200 && status < 400) {
+                    chrome.storage.local.set({ 'openBdResponse': xhr.responseText }, function() {
+                        console.log(`openBdResponse set to ${xhr.responseText}`);
+                    });
+                } else {
+                    throw new Error('Open BD APIからのGETエラー');
+                }
+            }
+        };
+        xhr.send();
+        // responseの内容を表示するための result.html を別タブで表示する
         chrome.tabs.create({
             'url': chrome.runtime.getURL('result.html'),
             'active': true
--- src/result.js
+++ src/result.js
@@ -1,1 +1,68 @@
-let pageOpenBd = document.getElementById('openBDcontent');
+window.addEventListener('load', function () {
+    // openBD APIから取得した取得したデータをchrome.storageから読み込む
+    chrome.storage.local.get(['openBdUrl', 'openBdResponse'], function (result) {
+        let url = result.openBdUrl;
+        let response = JSON.parse(result.openBdResponse);
+        // result.htmlへの反映処理
+        returnBookInfo(url, response);
+    });
+});
+
+/**
+ * result.htmlへの反映処理
+ * @param {string} openBdUrl 当該書籍に関するopenBDのURL
+ * @param {array} openBdResponse 当該書籍に関するopenBD登録情報 
+ */
+function returnBookInfo(openBdUrl, openBdResponse) {
+    try {
+        var bookInfo = openBdResponse.map(element => {
+            element = element || { 'summary': {} };
+            let bookSummary = {
+                'isbn': element.summary.isbn || 'NA',
+                'title': element.summary.title || 'NA',
+                'volume': element.summary.volume || 'NA',
+                'series': element.summary.series || 'NA',
+                'author': element.summary.author || 'NA',
+                'publisher': element.summary.publisher || 'NA',
+                'pubdate': element.summary.pubdate || 'NA',
+                'coverUrl': element.summary.cover || 'NA',
+                'coverUrlImg': (element.summary.cover ? `<img src="${element.summary.cover}">` : '<p>NA</p>'),
+                'coverUrlAhref': (element.summary.cover ? `<a href="${element.summary.cover}">${element.summary.cover}</a>` : '<p>NA</p>'),
+                'openBDUrl': openBdUrl
+            };
+            return bookSummary;
+        });
+        if (!bookInfo.length) {
+            throw new Error('該当する書籍情報がありません。');
+        } else if (bookInfo.length > 1) {
+            throw new Error('2つ以上の書籍情報が取得されました。選択したISBNを確認してください。');
+        }
+        bookInfoObj = bookInfo[0];
+        // 書影
+        document.getElementById('bookCover').innerHTML = bookInfoObj.coverUrlImg;
+        // 書籍の概要
+        document.getElementById('bookSummary').innerHTML =
+            `<ul>
+            <li><strong>ISBN: </strong>${bookInfoObj.isbn}</li>
+            <li><strong>書籍名: </strong>${bookInfoObj.title}</li>
+            <li><strong>Volume: </strong>${bookInfoObj.volume}</li>
+            <li><strong>Series: </strong>${bookInfoObj.series}</li>
+            <li><strong>著者: </strong>${bookInfoObj.author}</li>
+            <li><strong>出版社: </strong>${bookInfoObj.publisher}</li>
+            <li><strong>発行日（yyyy-MM）: </strong>${bookInfoObj.pubdate}</li>
+            <li><strong>書影URL: </strong>${bookInfoObj.coverUrlAhref}</li>
+            </ul>`;
+        // openBDでの登録内容
+        document.getElementById('openBdContent').textContent = JSON.stringify(openBdResponse);
+        // openBDのURL
+        document.getElementById('openBdUrl').innerHTML = `<a href="${openBdUrl}">${openBdUrl}</a>`;
+        // 後処理（chrome.storage内の情報を削除）
+        chrome.storage.local.remove(['openBdUrl', 'openBdResponse'], function () {
+            console.log('Reset: openBdUrl and openBdResponse');
+        })
+    } catch (error) {
+        let errorMessage = document.createElement('errorMessage');
+        errorMessage.textContent = `${error.message} - ${error.stack}`;
+        document.getElementById('pre').appendChild(errorMessage);
+    }
+}
