3ae40a7c55cbcc8b89b11ad4e14a5903fb4a88a1
ttsukagoshi <55706659+ttsukagoshi@users.noreply.github.com>
2020-11-27 16:28:33 +0900

    Add line break for better visibility

--- src/background.js
+++ src/background.js
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 // Global variable(s)
 
 const OPENBD_API_VERSION = 'v1'; // openBDのAPIバージョン。 https://openbd.jp/
